9dd74e892f4add4326ed0ac64e3ff38968cf8096
Mark Wong <markwong.digital@gmail.com>
2023-12-22 03:58:48 -0800

    Add background decoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import BackgroundDecoration from "./components/decorations/BackgroundDecoration";
+
 function App() {
 	return (
-		<>
-			<div className="">Portfolio</div>
-		</>
+		<div className="bg-gray-50 text-gray-950">
+			<BackgroundDecoration />
+			Portfolio
+		</div>
 	);
 }
 
