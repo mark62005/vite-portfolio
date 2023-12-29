7b71361028b1bc7a20eec53e07948ee1a6172ac7
Mark Wong <markwong.digital@gmail.com>
2023-12-29 03:12:59 -0800

    Add Back To Top button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import About from "./components/About";
+import BackToTopButton from "./components/BackToTopButton";
 import Contact from "./components/Contact";
 import BackgroundDecoration from "./components/decorations/BackgroundDecoration";
 import Experiences from "./components/Experiences";
@@ -11,26 +13,60 @@ import SectionDivider from "./components/ui/SectionDivider";
 import ActiveSectionContextProvider from "./context/ActiveSectionContext";
 
 function App() {
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	const handleScroll = () => {
+		if (
+			document.body.scrollTop > 20 ||
+			document.documentElement.scrollTop > 20
+		) {
+			setIsVisible(true);
+		} else {
+			setIsVisible(false);
+		}
+	};
+
+	const backToTop = () => {
+		document.documentElement.style.scrollBehavior = "smooth";
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	};
+
 	return (
-		<div
-			id="home"
-			className="relative h-[1000px] pt-28 sm:pt-36"
-		>
-			<ActiveSectionContextProvider>
-				<Header />
-				<main className="flex flex-col items-center px-4">
-					<BackgroundDecoration />
-					<Hero />
-					<SectionDivider />
-					<About />
-					<Projects />
-					<Skills />
-					<Experiences />
-					<Contact />
-				</main>
-				<Footer />
-			</ActiveSectionContextProvider>
-		</div>
+		<>
+			<div
+				id="home"
+				className="relative pt-28 sm:pt-36"
+			>
+				<ActiveSectionContextProvider>
+					<Header />
+					<main className="static flex flex-col items-center px-4">
+						<Hero />
+						<SectionDivider />
+						<About />
+						<Projects />
+						<Skills />
+						<Experiences />
+						<Contact />
+					</main>
+					<Footer />
+				</ActiveSectionContextProvider>
+				<BackToTopButton
+					isVisible={isVisible}
+					backToTop={backToTop}
+				/>
+
+				<BackgroundDecoration />
+			</div>
+		</>
 	);
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="mb-10 px-4 text-center text-gray-500">
+		<footer className="mb-5 px-4 text-center text-gray-500">
 			<small className="mb-2 block text-xs">
 				&copy; 2023 Ka Ho Wong. All rights reserved.
 			</small>
